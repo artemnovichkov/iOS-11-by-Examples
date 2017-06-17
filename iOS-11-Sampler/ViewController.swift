9d98f589618566b3b5107e8443bb7f9d16812855
Artem Novichkov <novichkoff93@gmail.com>
2017-06-17 17:02:54 +0600

    Initial Commit

---
+++ iOS-11-Sampler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-11-Sampler
+//
+//  Created by Artem Novichkov on 17/06/2017.
+//  Copyright © 2017 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
