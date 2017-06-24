d91342a2bddc36ea675baf0e6c57364913c078df
Artem Novichkov <novichkoff93@gmail.com>
2017-06-24 15:21:57 +0600

    Add layers for landmarks

--- iOS-11-by-Examples/Vision/Landmarks/LandmarksViewController.swift
+++ iOS-11-by-Examples/Vision/Landmarks/LandmarksViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 import Vision
 
 class LandmarksViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let request = VNDetectFaceLandmarksRequest { [unowned self] request, error in
             self.handle(request, error: error)
         }
-        let image = #imageLiteral(resourceName: "face").cgImage!
-        let handler = VNImageRequestHandler(cgImage: image, options: [:])
+        let image = #imageLiteral(resourceName: "face")
+        imageView.image = image
+        let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
         do {
             try handler.perform([request])
         } catch {
@@ -33,13 +36,54 @@ class LandmarksViewController: UIViewController {
         guard let observations = request.results as? [VNFaceObservation] else {
             return
         }
+        
         for observation in observations {
             guard let landmarks = observation.landmarks else {
                 continue
             }
-            let boundingBox = observation.boundingBox
-            print(landmarks.landmarkRegions)
+            
+            let faceRect = observation.boundingBox
+            let image = imageView.image!
+            let convertedFaceRect = CGRect(x: image.size.width * faceRect.origin.x,
+                                           y: image.size.height * (1 - faceRect.origin.y - faceRect.height),
+                                           width: image.size.width * faceRect.width,
+                                           height: image.size.height * faceRect.height)
+            
+            let boundingBoxLayer = CAShapeLayer()
+            boundingBoxLayer.path = UIBezierPath(rect: convertedFaceRect).cgPath
+            boundingBoxLayer.fillColor = nil
+            boundingBoxLayer.strokeColor = UIColor.red.cgColor
+            imageView.layer.addSublayer(boundingBoxLayer)
+            
+            layers(for: landmarks.landmarkRegions, rect: convertedFaceRect).forEach { layer in
+                self.imageView.layer.addSublayer(layer)
+            }
         }
+    }
+    
+    func layers(for regions: [VNFaceLandmarkRegion2D], rect: CGRect) -> [CAShapeLayer] {
+        return regions.map { region in
+            let points = region.points.map { point in
+                return CGPoint(x: rect.minX + point.x * rect.width,
+                               y: rect.minY + (1 - point.y) * rect.height)
+            }
+            
+            let layer = self.layer(withPoints: points)
+            return layer
+        }
+    }
+    
+    func layer(withPoints points: [CGPoint]) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        layer.fillColor = nil
+        layer.strokeColor = UIColor.red.cgColor
+        let path = UIBezierPath()
+        path.move(to: points.first!)
+        points.forEach { point in
+            path.addLine(to: point)
+        }
+        layer.path = path.cgPath
+        return layer
     }
 }
 
@@ -50,6 +94,29 @@ extension VNFaceLandmarks2D {
         if let faceContour = faceContour {
             landmarkRegions.append(faceContour)
         }
+        if let leftEye = leftEye {
+            landmarkRegions.append(leftEye)
+        }
+        if let rightEye = rightEye {
+            landmarkRegions.append(rightEye)
+        }
+        if let nose = nose {
+            landmarkRegions.append(nose)
+        }
+        if let outerLips = outerLips {
+            landmarkRegions.append(outerLips)
+        }
         return landmarkRegions
     }
 }
+
+extension VNFaceLandmarkRegion2D {
+    
+    var points: [CGPoint] {
+        return (0..<pointCount).map { index in
+            let point = self.point(at: index)
+            return CGPoint(x: CGFloat(point.x), y: CGFloat(point.y))
+        }
+    }
+}
+
