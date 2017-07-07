d03a7fa6b417be6f83b71f251d11276bba119981
Artem Novichkov <novichkoff93@gmail.com>
2017-07-07 18:52:13 +0600

    Add example with trees

--- iOS-11-by-Examples/ARKit/ARKitViewController.swift
+++ iOS-11-by-Examples/ARKit/ARKitViewController.swift
@@ -12,14 +12,17 @@ import ARKit
 class ARKitViewController: UIViewController {
 
     @IBOutlet weak var sceneView: ARSCNView!
+    var treeNode: SCNNode?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self
         sceneView.showsStatistics = true
         
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/lowpoly_tree_sample.dae")!
         sceneView.scene = scene
+        treeNode = scene.rootNode.childNode(withName: "Tree_lp_11", recursively: true)
+        treeNode?.position = SCNVector3Make(0, 0, -1)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -32,6 +35,20 @@ class ARKitViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        guard let touch = touches.first else { return }
+        let results = sceneView.hitTest(touch.location(in: sceneView), types: [.featurePoint])
+        guard let hitFeature = results.last else { return }
+        let hitTransform = SCNMatrix4(hitFeature.worldTransform)
+        let hitPosition = SCNVector3Make(hitTransform.m41,
+                                         hitTransform.m42,
+                                         hitTransform.m43)
+        let newTreeNode = treeNode!.clone()
+        newTreeNode.position = hitPosition
+        sceneView.scene.rootNode.addChildNode(newTreeNode)
     }
 }
 
