2579a88cc1a70cb85cd1cd33bb7353c260c1811e
Artem Novichkov <novichkoff93@gmail.com>
2017-08-08 19:47:58 +0600

    Add template for MapKit example

---
+++ iOS-11-by-Examples/MapKit/MapKitViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MapKitViewController.swift
+//  iOS-11-by-Examples
+//
+//  Created by Artem Novichkov on 08/08/2017.
+//  Copyright © 2017 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapKitViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
